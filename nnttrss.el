ac1195e8d3487f8ebda0c43da9a391e12943c56f
Pedro Silva <psilva+git@pedrosilva.pt>
2013-04-02 15:41:00 +0100

    Add library headers boilerplate

--- nnttrss.el
+++ nnttrss.el
@@ -1,5 +1,32 @@
 ;;; nnttrss.el --- interfacing with Tiny Tiny RSS
 
+;; Copyright (C) 2013 Pedro Silva
+
+;; Author: Pedro Silva <psilva+git@pedrosilva.pt>
+;; Created: 01 April 2013
+;; Version: 0.0.1
+;; Keywords: news, local
+;; Package-Requires ((emacs "23.1"))
+
+;; This file is not part of GNU Emacs.
+
+;; nnttrss is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; nnttrss is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with nnttrss.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
 (require 'gnus)
 (require 'nnoo)
 (require 'nnheader)
